c83c94fb70bdcd984ac064524ad43b9a802a5bb8
yewton <yewton@gmail.com>
2013-07-17 01:46:55 +0900

    add examples for 15.1.1, 15.1.2

--- 15_case_classes_and_pattern_matching/src/main/scala/DemoApp.scala
+++ 15_case_classes_and_pattern_matching/src/main/scala/DemoApp.scala
@@ -1,20 +1,34 @@
 import net.yewton.exercise.scala.ex15._
 
 object DemoApp extends App {
-  println("""== Case Class Demo ==""")
-  println("""1. Factory method with the name of the class""")
-  val v = Var("x")
-  val op = BinOp("+", Number(1), v)
-  println(v)
-  println(op)
-  println("""|2. All the arguments of a case class implicitly
-             |get a val prefix, so they are maintained as fields""".stripMargin)
-  println(v.name)
-  println(op.lhs)
-  println("""3. They have "natural" implementations of methods `toString`, `hashCode`, and `equals`""")
-  println(op.rhs == Var("x"))
-  println("""4. A copy method to your class for making modified copies.""")
-  println("""- Making an BinOp instance with operator "-" from an existing instance having "+"""")
-  println(op.copy(operator = "-"))
-  println("""== EOD ==""")
+  def demo(title :String)(f: => Unit): (() => Unit) = { () =>
+    println(s"""$title""")
+    f
+    println("""== EOD ==""")
+  }
+  def demo1 = demo("15.1.1 Case Class Demo") {
+    println("""1. Factory method with the name of the class""")
+    val v = Var("x")
+    val op = BinOp("+", Number(1), v)
+    println(v)
+    println(op)
+    println("""|2. All the arguments of a case class implicitly
+               |get a val prefix, so they are maintained as fields""".stripMargin)
+    println(v.name)
+    println(op.lhs)
+    println("""3. They have "natural" implementations of methods `toString`, `hashCode`, and `equals`""")
+    println(op.rhs == Var("x"))
+    println("""4. A copy method to your class for making modified copies.""")
+    println("""- Making an BinOp instance with operator "-" from an existing instance having "+"""")
+    println(op.copy(operator = "-"))
+  }
+  def demo2 = demo("15.1.2 Pattern Match Demo") {
+    println("We want to simplyfi -(-x), x + 0, x * 1...")
+    println(Expr.simplifyTop(UnOp("-", UnOp("-", Var("x")))))
+    println(Expr.simplifyTop(BinOp("+", Var("x"), Number(0))))
+    println(Expr.simplifyTop(BinOp("*", Var("x"), Number(1))))
+    println("Very well!!")
+  }
+  demo1()
+  demo2()
 }
--- 15_case_classes_and_pattern_matching/src/main/scala/net/yewton/exercise/scala/ex15/Expr.scala
+++ 15_case_classes_and_pattern_matching/src/main/scala/net/yewton/exercise/scala/ex15/Expr.scala
@@ -1,5 +1,14 @@
 package net.yewton.exercise.scala.ex15
 
+object Expr {
+  def simplifyTop(expr: Expr): Expr = expr match {
+    case UnOp("-", UnOp("-", e))  => e // -(-e) = e
+    case BinOp("+", e, Number(0)) => e // e + 0 = e
+    case BinOp("*", e, Number(1)) => e // e * 1 = e
+    case _ => expr
+  }
+}
+
 abstract class Expr
 case class Var(name: String) extends Expr
 case class Number(num: Double) extends Expr
